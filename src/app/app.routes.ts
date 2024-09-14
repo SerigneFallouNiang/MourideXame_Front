9b661a2243b606b835e05a1339410d4552dfd99f
SerigneFallouNiang <fallouniang776@gmail.com>
2024-09-14 23:05:29 +0000

    intégration de la page authentification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/auth/login/login.component';
+import { SignupComponent } from './Components/auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    //Route pour Uesr simple
+{path: "", pathMatch: "full", redirectTo: "login"},
+
+{path: "login", component : LoginComponent},
+{path: "register", component : SignupComponent},
+
+];
