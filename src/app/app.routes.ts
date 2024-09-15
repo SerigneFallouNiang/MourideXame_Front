3cf37fd44e0acefef95809fe331ec8d0681afad5
SerigneFallouNiang <fallouniang776@gmail.com>
2024-09-15 21:13:38 +0000

    intégration de la page liste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/auth/login/login.component';
 import { SignupComponent } from './Components/auth/signup/signup.component';
 import { AccueilComponent } from './Components/Apprenants/accueil/accueil.component';
+import { XassidasListeComponent } from './Components/Apprenants/xassidas-liste/xassidas-liste.component';
 
 export const routes: Routes = [
 
     //Route pour Uesr simple
-{path: "", pathMatch: "full", redirectTo: "accueil"},
+{path: "", pathMatch: "full", redirectTo: "xassidas"},
 
 {path: "login", component : LoginComponent},
 {path: "register", component : SignupComponent},
 {path: "accueil", component : AccueilComponent},
+{path: "xassidas", component : XassidasListeComponent},
 
 ];
