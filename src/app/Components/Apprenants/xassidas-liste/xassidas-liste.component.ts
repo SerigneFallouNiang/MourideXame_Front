6f9f6081247fb8282714ba1f4ea8adf6557a73f3
SerigneFallouNiang <fallouniang776@gmail.com>
2024-09-18 00:00:24 +0000

    manupilation des affichage

--- src/app/Components/Apprenants/xassidas-liste/xassidas-liste.component.ts
+++ src/app/Components/Apprenants/xassidas-liste/xassidas-liste.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-xassidas-liste',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './xassidas-liste.component.html',
   styleUrl: './xassidas-liste.component.css'
 })
 export class XassidasListeComponent {
+  isListVisible: boolean = false;
 
+  showList() {
+    this.isListVisible = true;
+  }
+
+  showCards() {
+    this.isListVisible = false;
+  } 
 }
