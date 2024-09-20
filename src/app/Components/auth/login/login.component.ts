83a94fa255d9b8a7008cb55d1936a937c95c0b29
SerigneFallouNiang <fallouniang776@gmail.com>
2024-09-20 00:02:12 +0000

    interceptor d'authentification

--- src/app/Components/auth/login/login.component.ts
+++ src/app/Components/auth/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../Services/auth.service';
+import { Router } from 'express';
+import { UserModel } from '../../../Models/user.model';
+import { AlertShowMessage } from '../../../Services/alretMessage';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,4 +14,54 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  //  // Injection de dependances 
+  //  private authService = inject(AuthService);
+  //  private router = inject(Router);
+ 
+  //  // Declaration des variables 
+  //  userObject: UserModel = {}; // un objet qui a pour type UserModel qui se trouve dans Models/user.model.ts
+  //  alertMessage: string = ""; // cette variable permettra de stocker la valeur de l'alerte
+ 
+ 
+  //  // Declaration des methodes 
+  //  login(){
+  //    console.log(this.userObject);
+  //    if(!this.userObject.email || !this.userObject.password){
+  //      this.alertMessage = "L'email et le mot de passe sont obligatoires"
+  //      // On importe la methode AlertShowMessage qui se trouve dans le dossier Services avant de l'utiliser
+  //      AlertShowMessage("alert-danger");
+  //    }
+  //    else{  
+  //      this.authService.login(this.userObject).subscribe(
+  //        (response:any) =>{
+  //          // console.log(response); 
+  //          if(response.success){
+  //            // Enregistrement du token et des insfos de l'utilisateur dans le localstorage 
+  //            localStorage.setItem("infos_Connexion", JSON.stringify(response));
+ 
+  //            // Redirection de l'utilisateur connecté vers la page lui concernant 
+  //            const user = response.user;
+  //            // console.log(user);            
+  //            if(user.role === "admin"){
+  //              this.router.navigateByUrl("/accuelMembre") // On les membres vers leur page d'accueil
+  //            } else {
+  //              this.router.navigateByUrl("/livres") // On redirige l'admin ou le personnel vers la liste des membres
+ 
+  //            }
+  //          }       
+  //        },
+  //        (error) =>{
+  //          console.log("les erreurs");
+  //          console.log(error.error.message);
+  //          this.alertMessage = error.error.message;
+  //          AlertShowMessage("alert-danger");
+ 
+  //        }
+  //      )
+ 
+  //    }
+     
+  //  }
+ 
+  
 }
--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { apiUrl } from "./apiUrl";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root"
 })
-export class AuthService {
 
-  constructor() { }
+export class AuthService {
+    private http = inject(HttpClient);
+
+    // Methode pour s'authetifier 
+    login(identifiant:any){
+        return this.http.post(`${apiUrl}/login`, identifiant);
+    }
+    
+    // Methode s'inscrire 
+    register(identifiant:any){
+        return this.http.post(`${apiUrl}/register`, identifiant);
+    }
+    
+    // Methode pour avoir le profil utilisateur 
+    getProfile(){
+        return this.http.get(`${apiUrl}/profile`);
+    }
+
+    // Methode pour rafraichir le token
+    refreshToken(){
+        return this.http.get(`${apiUrl}/refresh`);
+    }
+
+    // Methode pour se deconnecter 
+    logout(){
+        return this.http.get(`${apiUrl}/logout`);
+    }
 }
