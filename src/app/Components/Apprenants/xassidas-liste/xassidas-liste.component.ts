5cd384cc1c6b2441ab4ebc5a092cc58a84fad137
SerigneFallouNiang <fallouniang776@gmail.com>
2024-09-21 10:34:42 +0000

    recuperation des livre par categorie

--- src/app/Components/Apprenants/xassidas-liste/xassidas-liste.component.ts
+++ src/app/Components/Apprenants/xassidas-liste/xassidas-liste.component.ts
@@ -3,6 +3,7 @@ import { Component, NgModule, OnInit } from '@angular/core';
 import { NavbarApprenantComponent } from '../../heritage/navbar-apprenant/navbar-apprenant.component';
 import { ActivatedRoute } from '@angular/router';
 import { BookService } from '../../../Services/book.service';
+import { apiUrlStockage } from '../../../Services/apiUrlStockage';
 
 @Component({
   selector: 'app-xassidas-liste',
@@ -24,6 +25,7 @@ export class XassidasListeComponent implements OnInit{
 
   books: any[] = [];
   categoryName: string = '';
+  messageImage: string = "Aucune image pour ce categorie";
 
   constructor(
     private route: ActivatedRoute,
@@ -42,6 +44,14 @@ export class XassidasListeComponent implements OnInit{
       (data: any) => {
         this.books = data.books;
         this.categoryName = data.categoryName;
+         // Met à jour l'URL de l'image pour chaque catégorie
+         this.books.forEach(books => {
+          if (books.image) {
+            books.image = `${apiUrlStockage}/${books.image}`;
+          } else {
+            this.messageImage = "Aucune image pour cette catégorie";
+          }
+        });
       },
       error => {
         console.error('Error loading books:', error);
