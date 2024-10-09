67541859f68963c3ca6f8b553101a0fbd5dc6a88
SerigneFallouNiang <fallouniang776@gmail.com>
2024-10-09 11:20:12 +0000

    affichage détail utilisateur sur la page admin

--- src/app/Components/Admin/Utilisateurs/user-history-modal.component.ts
+++ src/app/Components/Admin/Utilisateurs/user-history-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RolesService } from '../../../Services/Admin/roles.service';
 
@@ -12,7 +12,7 @@ import { RolesService } from '../../../Services/Admin/roles.service';
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Historique de lecture</h5>
-            <button type="button" class="close" (click)="close()">
+            <button type="button" class="close" (click)="onClose()">
               <span>&times;</span>
             </button>
           </div>
@@ -28,14 +28,14 @@ import { RolesService } from '../../../Services/Admin/roles.service';
             <div *ngIf="!loading && books.length > 0">
               <div class="list-group">
                 <div *ngFor="let book of books" class="list-group-item">
-                  <h6 class="mb-1">{{book.titre}}</h6>
+                  <h6 class="mb-1">{{book.title}}</h6>
                   <p class="mb-1">{{book.description}}</p>
                 </div>
               </div>
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" (click)="close()">Fermer</button>
+            <button type="button" class="btn btn-secondary" (click)="onClose()">Fermer</button>
           </div>
         </div>
       </div>
@@ -44,24 +44,43 @@ import { RolesService } from '../../../Services/Admin/roles.service';
   styles: [`
     .modal {
       display: none;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
       background-color: rgba(0,0,0,0.5);
+      z-index: 1050;
     }
     .modal.show {
       display: block;
     }
+    .modal-dialog {
+      margin: 1.75rem auto;
+    }
   `]
 })
-export class UserHistoryModalComponent {
+export class UserHistoryModalComponent implements OnChanges {
   @Input() isVisible: boolean = false;
+  @Input() userId: string | null = null;
+  @Output() closeModal = new EventEmitter<void>();
+  
   loading: boolean = false;
   books: any[] = [];
 
   constructor(private rolesService: RolesService) {}
 
-  showHistory(userId: string) {
-    this.isVisible = true;
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['userId'] && changes['userId'].currentValue && this.isVisible) {
+      this.loadHistory();
+    }
+  }
+
+  loadHistory() {
+    if (!this.userId) return;
+    
     this.loading = true;
-    this.rolesService.getUserHistory(userId).subscribe({
+    this.rolesService.getUserHistory(this.userId).subscribe({
       next: (response) => {
         this.books = response.books;
         this.loading = false;
@@ -73,7 +92,7 @@ export class UserHistoryModalComponent {
     });
   }
 
-  close() {
-    this.isVisible = false;
+  onClose() {
+    this.closeModal.emit();
   }
 }
