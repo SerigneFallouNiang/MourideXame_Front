afee0bb3fa6ee714caba0e05734c1a83d37009f9
SerigneFallouNiang <fallouniang776@gmail.com>
2024-10-12 00:34:17 +0000

    responsive des formulaire d'authentification

--- src/app/Services/apiUrl.ts
+++ src/app/Services/apiUrl.ts
@@ -1,1 +1,2 @@
-export const apiUrl = "https://mouride.fallouniang776.simplonfabriques.com/api"
+export const apiUrl = "http://127.0.0.1:8000/api"
+// export const apiUrl = "https://mouride.fallouniang776.simplonfabriques.com/api"
--- src/app/Services/apiUrlStockage.ts
+++ src/app/Services/apiUrlStockage.ts
@@ -1,1 +1,2 @@
-export const apiUrlStockage = "https://mouride.fallouniang776.simplonfabriques.com/storage"
+export const apiUrlStockage = "http://127.0.0.1:8000/storage"
+// export const apiUrlStockage = "https://mouride.fallouniang776.simplonfabriques.com/storage"
