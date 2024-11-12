3a201c00bf084a2ed3221248d202356399a735b3
SerigneFallouNiang <fallouniang776@gmail.com>
2024-11-12 06:54:22 +0000

    changement des url

--- src/app/Services/apiUrl.ts
+++ src/app/Services/apiUrl.ts
@@ -1,5 +1,5 @@
 
 
 // export const apiUrl = "http://mouridedu.kesug.com/api"
-//export const apiUrl = "http://127.0.0.1:8000/api"
- export const apiUrl = "https://mouridxam.fallouniang776.simplonfabriques.com/api"
+// export const apiUrl = "http://127.0.0.1:8000/api"
+ export const apiUrl = "https://mouride.fallouniang776.simplonfabriques.com/api"
--- src/app/Services/apiUrlStockage.ts
+++ src/app/Services/apiUrlStockage.ts
@@ -1,5 +1,5 @@
 
 // export const apiUrlStockage = "http://mouridedu.kesug.com/storage"
-//export const apiUrlStockage = "http://127.0.0.1:8000/storage"
- export const apiUrlStockage = "https://mouridxam.fallouniang776.simplonfabriques.com/storage"
+// export const apiUrlStockage = "http://127.0.0.1:8000/storage"
+ export const apiUrlStockage = "https://mouride.fallouniang776.simplonfabriques.com/storage"
 
